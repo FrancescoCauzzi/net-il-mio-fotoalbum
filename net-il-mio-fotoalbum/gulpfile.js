00afea34c5c92b420968b79c43160f58bce6e993
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-10-11 14:25:42 +0200

    feat: scss added, main layout sorted

---
+++ net-il-mio-fotoalbum/gulpfile.js
@@ -0,0 +1,25 @@
+﻿
+
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+// Define the 'sass' task
+function compileSass(cb) {
+    return src('./wwwroot/css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('./wwwroot/css/compiled'));
+}
+
+// Define the 'watch' task
+function watchSass(cb) {
+    watch('./wwwroot/css/*.scss', series(compileSass));
+    cb();
+}
+
+// Export tasks
+
+exports.compileSass = compileSass;
+exports.watchSass = watchSass;
+exports.default = series(compileSass, watchSass);
+
+
