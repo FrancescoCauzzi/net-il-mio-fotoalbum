a23bafdaea82975b89da2b9740543d7bbe6adaf1
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-10-13 10:21:04 +0200

    feat: methods in the api-picture-controller implemented

--- net-il-mio-fotoalbum/Controllers/API/PicturesController.cs
+++ net-il-mio-fotoalbum/Controllers/API/PicturesController.cs
@@ -1,26 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Database;
+using net_il_mio_fotoalbum.Models;
+using net_il_mio_fotoalbum.Models.DatabaseModels;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace net_il_mio_fotoalbum.Controllers.API
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class PicturesController : ControllerBase
     {
+        private IRepository<Picture, PictureFormModel> _repositoryPicture;
+        private IRepository<Category, CategoryFormModel> _repositoryCategory;
+
+
+        public PicturesController(IRepository<Picture, PictureFormModel> repositoryPicture, IRepository<Category, CategoryFormModel> repositoryCategory)
+        {
+            _repositoryPicture = repositoryPicture;
+            _repositoryCategory = repositoryCategory;
+        }
+
+        
         // GET: api/<PicturesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult GetAllPictures()
         {
-            return new string[] { "value1", "value2" };
+            List<Picture> pictureList = _repositoryPicture.GetAll();
+            return Ok(pictureList);
         }
 
         // GET api/<PicturesController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult GetPictureById(int id)
         {
-            return "value";
+            Picture picture = _repositoryPicture.GetEntityById(id);
+
+            if (picture != null)
+            {
+                return Ok(picture);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
+
+        [HttpGet]
+        public IActionResult SearchPictures(string? search)
+        {
+            List<Picture> foundedPictures = new List<Picture>();
+            if (search == null)
+            {
+                foundedPictures = _repositoryPicture.GetAll();
+            }
+            else
+            {
+                foundedPictures = _repositoryPicture.GetEntities(search);
+            }
+
+            return Ok(foundedPictures);
+
+        }
+
+        // here we start the POST requests,they are not necessary for the exercise, they could be implemented later
+        // add a new picture to the db
 
         // POST api/<PicturesController>
         [HttpPost]
---
+++ net-il-mio-fotoalbum/Migrations/20231013080250_ContactsModelAndContrAdded.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace net_il_mio_fotoalbum.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContactsModelAndContrAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImageFile",
+                table: "Pictures",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImageFile",
+                table: "Pictures",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+        }
+    }
+}
